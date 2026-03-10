c69005dbae6c91f1668aeaf53139978c43df6cf2
vinijesuss <viniciusjesus@revio.com.br>
2026-03-10 13:48:58 -0300

    Tela de Cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import { ToastProvider } from "./context/ToastContext.tsx";
+import Register from "./pages/register/Register.tsx";
 // import { SidebarProvider } from "./context/SidebarContext.tsx";
-import { Page_Title } from "./hooks/Page_Title.tsx";
+// import { Page_Title } from "./hooks/Page_Title.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Navigate to="login" />,
+  },
+  {
+    path: "/login",
     element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
--- src/pages/login/Menu_Login.tsx
+++ src/pages/login/Menu_Login.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import LoginForm from "./components/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 const Menu_Login = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="from-primary to-accent flex min-h-screen w-full flex-col items-center justify-center bg-linear-to-r bg-cover bg-center p-4 md:flex-row md:p-0">
-      <section className="bg-background rounded-lg px-10 py-40 shadow-md">
-        <div className="">
-          <h1 className="text-center text-4xl font-bold">
+      <section className="bg-background min-w-xl rounded-lg px-10 py-40 shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">
             Bem-<span className="text-accent">Vindo</span>
           </h1>
           <p className="text-text-secundary mt-4">
@@ -25,14 +28,17 @@ const Menu_Login = () => {
         <div className="mt-6 text-center text-sm">
           <p>
             Não tem uma conta?{" "}
-            <span className="text-primary hover:border-primary mt-4 inline-block border-b-2 border-transparent pb-0.5 text-sm transition-colors">
+            <button
+              onClick={() => navigate("/register")}
+              className="text-primary hover:border-primary mt-4 inline-block border-b-2 border-transparent pb-0.5 text-sm transition-colors"
+            >
               Crie uma conta
-            </span>
+            </button>
           </p>
-          <p className="text-text-secundary mt-4 max-w-sm">
+          <p className="text-text-secundary mt-4 inline-block max-w-sm">
             Ao clicar em continuar, você concorda com os{" "}
             <span className="text-dark">Termos de Uso</span> e{" "}
-            <span className="text-dark">Políticas de Privacidade</span>
+            <span className="text-dark">Políticas de Privacidade.</span>
           </p>
         </div>
       </section>
